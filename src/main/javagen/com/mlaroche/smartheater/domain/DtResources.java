84fab610d45bc5f0f0eb4a4b69002f59c9af2fbd
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2018-09-08 16:41:44 +0200

    license ok

--- src/main/javagen/com/mlaroche/smartheater/domain/DtResources.java
+++ src/main/javagen/com/mlaroche/smartheater/domain/DtResources.java
@@ -1,5 +1,5 @@
 /**
- * Copyright [2018] [Matthieu Laroche - France]
+ * Copyright 2018 Matthieu Laroche - France
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
